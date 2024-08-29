c073552ee9f38d45514778bcd9c6779200ae9ec3
mytestaccount-1 <abhishekrathi639@gmail.com>
2024-08-29 16:48:44 +0530

    add read aloud feature

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const inputText = document.getElementById("input-text")
 const result = document.getElementById("result")
 const lowerButton = document.getElementById("lower-button")
 const upperButton = document.getElementById("upper-button")
+const speakButton = document.getElementById("speak")
 
 inputText.addEventListener('input',()=>{
     result.innerText = inputText.value
@@ -16,3 +17,19 @@ lowerButton.addEventListener('click',()=>{
     result.innerText = inputText.value.toLowerCase()
     inputText.value = inputText.value.toLowerCase()
 })
+
+speakButton.addEventListener('click',()=>{
+    speak()
+})
+
+function speak(){
+    const text =  result.innerText;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = getFemaleVoice();
+    speechSynthesis.speak(utterance);
+}
+
+function getFemaleVoice() {
+    const voiceIndex = 1;
+    return speechSynthesis.getVoices()[voiceIndex];
+  }

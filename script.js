53587ca4a2b896a885725acc9da5b21352af2d32
mytestaccount-1 <abhishekrathi639@gmail.com>
2024-08-29 18:02:22 +0530

    voice changeover

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-const inputText = document.getElementById("input-text")
-const result = document.getElementById("result")
-const lowerButton = document.getElementById("lower-button")
-const upperButton = document.getElementById("upper-button")
-const speakButton = document.getElementById("speak")
+const inputText = document.getElementById("input-text");
+const result = document.getElementById("result");
+const lowerButton = document.getElementById("lower-button");
+const upperButton = document.getElementById("upper-button");
+const speakButton = document.getElementById("speak");
 
-inputText.addEventListener('input',()=>{
-    result.innerText = inputText.value
-})
+inputText.addEventListener("input", () => {
+  result.innerText = inputText.value;
+});
 
-upperButton.addEventListener('click',()=>{
-    result.innerText = inputText.value.toUpperCase()
-    inputText.value = inputText.value.toUpperCase()
-})
+// change case
+upperButton.addEventListener("click", () => {
+  result.innerText = inputText.value.toUpperCase();
+  inputText.value = inputText.value.toUpperCase();
+});
 
-lowerButton.addEventListener('click',()=>{
-    result.innerText = inputText.value.toLowerCase()
-    inputText.value = inputText.value.toLowerCase()
-})
+lowerButton.addEventListener("click", () => {
+  result.innerText = inputText.value.toLowerCase();
+  inputText.value = inputText.value.toLowerCase();
+});
 
-speakButton.addEventListener('click',()=>{
-    speak()
-})
+// read aloud when clicked
+speakButton.addEventListener("click", () => {
+  speak();
+});
 
-function speak(){
-    const text =  result.innerText;
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.voice = getFemaleVoice();
-    speechSynthesis.speak(utterance);
+function speak() {
+  const text = result.innerText;
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.voice = getVoice();
+  console.log(utterance);
+  speechSynthesis.speak(utterance);
 }
 
-function getFemaleVoice() {
-    const voiceIndex = 1;
-    return speechSynthesis.getVoices()[voiceIndex];
-  }
+function getVoice() {
+  const voiceIndex = 1;
+  return speechSynthesis.getVoices()[voiceIndex];
+}
+
+// translate the text entered into a language selected by the user
